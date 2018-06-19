e50ba64f4b5971299375bdb513bd264fbb5b4861
hyj <huyajuan@taiwu.com>
2018-06-19 16:59:29 +0800

    log

---
+++ ModuleManagerProtocol/ModuleManagerProtocol/ModuleManagerProtocol.h
@@ -0,0 +1,13 @@
+//
+//  ModuleManagerProtocol.h
+//  ModuleManagerProtocol
+//
+//  Created by hyj on 2018/6/19.
+//  Copyright © 2018年 hyj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ModuleManagerProtocol : NSObject
+
+@end
